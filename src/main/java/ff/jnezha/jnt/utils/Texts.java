4e7161b00ce5932bd786cda67df084c6d0113c52
sanbo <sanbo.xyz@gmail.com>
2020-12-10 09:59:08 +0800

    增加并调整工具类

--- src/main/java/ff/jnezha/jnt/utils/Texts.java
+++ src/main/java/ff/jnezha/jnt/utils/Texts.java
@@ -1,10 +1,154 @@
 package ff.jnezha.jnt.utils;
 
+import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Texts {
 
-    public static boolean isEmpty(String data) {
-        return null == data || "" == data || data.length() == 0;
+
+    /**
+     * Returns true if the string is null or 0-length.
+     *
+     * @param str the string to be examined
+     * @return true if str is null or zero length
+     */
+    public static boolean isEmpty(CharSequence str) {
+        if (str == null || str.length() == 0)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Returns whether the given CharSequence contains only digits.
+     */
+    public static boolean isDigitsOnly(CharSequence str) {
+        final int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns whether the given CharSequence contains any printable characters.
+     */
+    public static boolean isGraphic(CharSequence str) {
+        final int len = str.length();
+        for (int i = 0; i < len; i++) {
+            int gc = Character.getType(str.charAt(i));
+            if (gc != Character.CONTROL && gc != Character.FORMAT && gc != Character.SURROGATE
+                    && gc != Character.UNASSIGNED && gc != Character.LINE_SEPARATOR
+                    && gc != Character.PARAGRAPH_SEPARATOR && gc != Character.SPACE_SEPARATOR) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns whether this character is a printable character.
+     */
+    public static boolean isGraphic(char c) {
+        int gc = Character.getType(c);
+        return gc != Character.CONTROL && gc != Character.FORMAT && gc != Character.SURROGATE
+                && gc != Character.UNASSIGNED && gc != Character.LINE_SEPARATOR && gc != Character.PARAGRAPH_SEPARATOR
+                && gc != Character.SPACE_SEPARATOR;
+    }
+
+    public static boolean isPrintableAscii(final char c) {
+        final int asciiFirst = 0x20;
+        final int asciiLast = 0x7E; // included
+        return (asciiFirst <= c && c <= asciiLast) || c == '\r' || c == '\n';
+    }
+
+    public static boolean isPrintableAsciiOnly(final CharSequence str) {
+        final int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (!isPrintableAscii(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
+    public static boolean isNumberByJavaAPI(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNumberByPattern(String str) {
+        // 该正则表达式可以匹配所有的数字 包括负数
+//        -?[0-9]+\\.?[0-9]*这个表达式在匹配字符串1.、222.时，确实会出现问题。
+//        网友的建议改为：-?[0-9]+(\\.[0-9]+)?。
+        Pattern pattern = Pattern.compile("-?[0-9]+(\\.[0-9]+)?");
+        String bigStr;
+        try {
+            bigStr = new BigDecimal(str).toString();
+        } catch (Exception e) {
+            return false;//异常 说明包含非数字。
+        }
+
+        Matcher isNum = pattern.matcher(bigStr); // matcher是全匹配
+        if (!isNum.matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isNumeric(String str) {
+        String bigStr;
+        try {
+            bigStr = new BigDecimal(str).toString();
+        } catch (Exception e) {
+            return false;//异常 说明包含非数字。
+        }
+        return true;
+    }
+
+
+    /**
+     * 中文转Unicode
+     *
+     * @param string
+     * @return
+     */
+    public static String strToUnicode(String string) {
+        char[] utfBytes = string.toCharArray();
+        String unicodeBytes = "";
+        for (int i = 0; i < utfBytes.length; i++) {
+            String hexB = Integer.toHexString(utfBytes[i]);
+            if (hexB.length() <= 2) {
+                hexB = "00" + hexB;
+            }
+            unicodeBytes = unicodeBytes + "\\u" + hexB;
+        }
+        return unicodeBytes;
+    }
+
+    /**
+     * Unicode转中文
+     *
+     * @param string
+     * @return
+     */
+    public static String unicodetoString(String string) {
+        Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+        Matcher matcher = pattern.matcher(string);
+        char ch;
+        while (matcher.find()) {
+            ch = (char) Integer.parseInt(matcher.group(2), 16);
+            string = string.replace(matcher.group(1), ch + "");
+        }
+        return string;
+    }
 }
