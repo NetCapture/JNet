9814acc4db78af8fee95f457025c72099d6dbb60
sanbo <sanbo.xyz@gmail.com>
2020-12-10 16:06:09 +0800

    更新打包文件

--- src/main/java/ff/jnezha/jnt/utils/SSLConfig.java
+++ src/main/java/ff/jnezha/jnt/utils/SSLConfig.java
@@ -7,7 +7,7 @@ import javax.net.ssl.*;
  * @Description: SSL需要的配置, 已经包含: 请求获取SSL工厂和 证书验证器
  * @Version: 1.0
  * @Create: 2020-12-08 15:18:15
- * @author: sanbo
+ * @Author: sanbo
  */
 public class SSLConfig {
     public static final HostnameVerifier NOT_VERYFY = new HostnameVerifier() {
