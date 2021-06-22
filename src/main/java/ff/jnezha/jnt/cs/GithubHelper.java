418e701614804507d383c655836efb3b610a06f6
sanbo <sanbo.xyz@gmail.com>
2021-06-22 13:45:08 +0800

    update files

--- src/main/java/ff/jnezha/jnt/cs/GithubHelper.java
+++ src/main/java/ff/jnezha/jnt/cs/GithubHelper.java
@@ -1,9 +1,11 @@
 package ff.jnezha.jnt.cs;
 
 import ff.jnezha.jnt.Jnt;
+import ff.jnezha.jnt.utils.FileUtils;
 import ff.jnezha.jnt.utils.HttpType;
 import ff.jnezha.jnt.utils.TextUtils;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.HashMap;
@@ -20,6 +22,9 @@ import java.util.regex.Pattern;
  */
 public class GithubHelper {
 
+
+    public static void main(String[] args) {
+ }
 
     public static void updateContent(String owner, String repo, String path, String token, String contentWillBase64, String commitMsg) {
         updateContent(owner, repo, path, token, contentWillBase64, commitMsg, "", "");
@@ -89,15 +94,26 @@ public class GithubHelper {
      * @param commitMsg         提交msg
      */
     public static String createFile(String owner, String repo, String path, String token, String contentWillBase64, String commitMsg) {
-        return createFile(owner, repo, path, token, contentWillBase64, commitMsg, "", "");
+        return createFile(true, owner, repo, path, token, contentWillBase64, commitMsg, "", "");
+    }
+
+    public static String createFile(String owner, String repo, String path, String token, File file, String commitMsg) {
+        String content = FileUtils.getBase64FromFile(file);
+        return createFile(false, owner, repo, path, token, content, commitMsg, "", "");
     }
 
     private static Pattern downloadUrlPattern = Pattern.compile("\"download_url\": *\"([^\"]+)\"");
     private static Pattern getSha = Pattern.compile("\"sha\": *\"([^\"]+)\"");
 
-    public static String createFile(String owner, String repo, String path, String token, String contentWillBase64, String commitMsg, String username, String email) {
+
+    public static String createFile(boolean isNeedBase64, String owner, String repo, String path, String token, String uploadContent, String commitMsg, String username, String email) {
         try {
-            String content = Base64.getEncoder().encodeToString(contentWillBase64.getBytes(StandardCharsets.UTF_8));
+            String content = null;
+            if (isNeedBase64) {
+                content = Base64.getEncoder().encodeToString(uploadContent.getBytes(StandardCharsets.UTF_8));
+            } else {
+                content = uploadContent;
+            }
             // 据RFC 822规定，每76个字符，还需要加上一个回车换行
             // 有时就因为这些换行弄得出了问题，解决办法如下，替换所有换行和回车
             // result = result.replaceAll("[\\s*\t\n\r]", "");
@@ -120,11 +136,6 @@ public class GithubHelper {
             int timeout = 10 * 1000;
             String res = Jnt.request(HttpType.PUT, timeout, uploadUrl, null, reqHeaderMap, data);
 
-//            JSONObject obj = JSON.parseObject(res);
-//            if (obj.size() > 0) {
-//                return obj.getJSONObject("content").getString("download_url");
-//            }
-
             Matcher matcher = downloadUrlPattern.matcher(res.toString());
             while (matcher.find()) {
                 return matcher.group(1);
@@ -136,21 +147,43 @@ public class GithubHelper {
         return "";
     }
 
+    /**
+     *  这个API只支持1MB以内的获取
+     *  <code>
+     *      //https://api.github.com/repos/hhhaiai/ManagerApk/contents/VMOS-Pro_1.3.apk
+     *{
+     *   "message": "This API returns blobs up to 1 MB in size. The requested blob is too large to fetch via the API, but you can use the Git Data API to request blobs up to 100 MB in size.",
+     *   "errors": [
+     *     {
+     *       "resource": "Blob",
+     *       "field": "data",
+     *       "code": "too_large"
+     *     }
+     *   ],
+     *   "documentation_url": "https://docs.github.com/rest/reference/repos#get-repository-content"
+     * }
+     *  <code/>
+     * @param owner
+     * @param repo
+     * @param path
+     * @param token
+     * @return
+     */
     public static String getSha(String owner, String repo, String path, String token) {
         try {
             String base = "https://api.github.com/repos/%s/%s/contents%s";
             String requestUrl = String.format(base, owner, repo, path, token);
 //            System.out.println(requestUrl);
             Map<String, String> reqHeaderMap = new HashMap<String, String>();
-            reqHeaderMap.put("Content-Type", "application/json;charset=UTF-8");
 //        reqHeaderMap.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
-            reqHeaderMap.put("User-Agent", "Github");
-            reqHeaderMap.put("Authorization", "token " + token)
-//        reqHeaderMap.put("Accept-Encoding", "gzip, deflate, br");
-//        reqHeaderMap.put("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
+            reqHeaderMap.put("User-Agent", "Github createFile By Java");
+            reqHeaderMap.put("Content-Type", "application/json;charset=UTF-8");
+            reqHeaderMap.put("Authorization", "token " + token);
+        reqHeaderMap.put("Accept-Encoding", "gzip, deflate, br");
+        reqHeaderMap.put("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
             int timeout = 10 * 1000;
             String result = Jnt.request(HttpType.GET, timeout, requestUrl, null, reqHeaderMap, null);
-
+            System.out.println("getSha result:" +result);
             Matcher matcher = getSha.matcher(result.toString());
             while (matcher.find()) {
                 return matcher.group(1);
