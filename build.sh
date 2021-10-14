d240f2377022a78169a165d4f2e49d5292072303
sanbo.xyz <sanbo.xyz@gmail.com>
2021-10-14 16:58:02 +0800

    增加编译脚本

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export GPG_TTY=$(tty)
+
+
+fileName="$0"
+
+if [ "$1" == "deploy" ]; then
+  mvn clean deploy
+elif [ "$1" == "upload" ]; then
+  mvn clean deploy
+else
+  mvn clean install
+fi
