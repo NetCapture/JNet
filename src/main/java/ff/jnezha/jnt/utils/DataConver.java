5297f58a94785ad029b1b999966f31f41dcb0089
sanbo <sanbo.xyz@gmail.com>
2022-08-24 13:26:38 +0800

    update sth. at 2022-08-24 13:26:38

--- src/main/java/ff/jnezha/jnt/utils/DataConver.java
+++ src/main/java/ff/jnezha/jnt/utils/DataConver.java
@@ -2,7 +2,6 @@ package ff.jnezha.jnt.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
