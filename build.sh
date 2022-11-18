f8c02634418fba4d09226a20746e5d7cf684ccf2
sanbo <sanbo.xyz@gmail.com>
2022-11-18 16:13:38 +0800

    update sth. at 2022-11-18 16:13:38

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 export GPG_TTY=$(tty)
 
-fileName="$0"
+#fileName="$0"
 #https://issues.sonatype.org/browse/OSSRH-66257
-export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
+#export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
 if [ "$1" == "deploy" ]; then
   mvn clean deploy
 elif [ "$1" == "upload" ]; then
